39e176c845c5dbc9fe2a0fb65b42e7f3fc8badac
Benny <hotman0901@gmail.com>
2019-05-22 01:16:49 +0800

    add comment for action

--- src/redux/actions/todo.js
+++ src/redux/actions/todo.js
@@ -8,6 +8,15 @@ import * as types from '../actionTypes/todo';
 // 使用async，必須設置applyMiddleware
 export const addTodo = payload => dispatch => dispatch({ type: types.ADD_TODO, payload });
 
+// 可參考下列寫法
+// export const xxx = () => (dispatch) => {
+//     dispatch({ type: REQUEST_ROBOT_PENDING});
+//     fetch('https://xxxxx.xxx.xx')
+//     .then(res => res.json())
+//     .then(data => dispach({ type: REQUEST_ROBOT_SUC}, payload: data))
+//     .catch( err => dispach({ type: REQUEST_ROBOTERR}, payload: err) )
+// }
+
 export const deleteTodo = payload => ({
     type: types.DELETE_TODO,
     payload,

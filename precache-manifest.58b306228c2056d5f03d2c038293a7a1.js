e766400bdf40eff5fc008db0cf010ef8bc097855
benny.hsieh <benny.hsieh@isuncloud.com>
2019-05-22 01:39:24 +0800

    Updates

---
+++ precache-manifest.58b306228c2056d5f03d2c038293a7a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86ba55d4b90dc11f792c93b8d45d105",
+    "url": "/react-start-kit/index.html"
+  },
+  {
+    "revision": "dec62dcd583d2c779c3d",
+    "url": "/react-start-kit/static/css/main.c7da2ae8.chunk.css"
+  },
+  {
+    "revision": "b5b9709084f75f632373",
+    "url": "/react-start-kit/static/js/2.ed9dc535.chunk.js"
+  },
+  {
+    "revision": "dec62dcd583d2c779c3d",
+    "url": "/react-start-kit/static/js/main.fb5d441d.chunk.js"
+  },
+  {
+    "revision": "b57ef1c723f7a8865919",
+    "url": "/react-start-kit/static/js/runtime~main.2e77f99e.js"
+  }
+]);
